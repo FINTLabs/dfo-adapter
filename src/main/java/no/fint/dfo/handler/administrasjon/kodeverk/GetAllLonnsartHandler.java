7fc53c4f548eb40093656eb6718b1263ba7e908b
Frode Sjovatsen <frode@fintlabs.no>
2021-02-22 19:11:56 +0100

    added missing @Service

--- src/main/java/no/fint/dfo/handler/administrasjon/kodeverk/GetAllLonnsartHandler.java
+++ src/main/java/no/fint/dfo/handler/administrasjon/kodeverk/GetAllLonnsartHandler.java
@@ -4,10 +4,12 @@ import no.fint.dfo.handler.Handler;
 import no.fint.event.model.Event;
 import no.fint.model.administrasjon.kodeverk.KodeverkActions;
 import no.fint.model.resource.FintLinks;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Set;
 
+@Service
 public class GetAllLonnsartHandler implements Handler {
     @Override
     public void accept(Event<FintLinks> fintLinksEvent) {
--- src/main/java/no/fint/dfo/handler/administrasjon/personal/GetUpdateVariabellonnHandler.java
+++ src/main/java/no/fint/dfo/handler/administrasjon/personal/GetUpdateVariabellonnHandler.java
@@ -4,10 +4,12 @@ import no.fint.dfo.handler.Handler;
 import no.fint.event.model.Event;
 import no.fint.model.administrasjon.personal.PersonalActions;
 import no.fint.model.resource.FintLinks;
+import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Set;
 
+@Service
 public class GetUpdateVariabellonnHandler implements Handler {
     @Override
     public void accept(Event<FintLinks> fintLinksEvent) {
