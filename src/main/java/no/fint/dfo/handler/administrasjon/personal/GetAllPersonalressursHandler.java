2eb126d9cb7945460b1b770809a77be11a5e0d8c
Frode Sjovatsen <frode@fintlabs.no>
2021-02-23 19:32:10 +0100

    added some sample data

--- src/main/java/no/fint/dfo/handler/administrasjon/personal/GetAllPersonalressursHandler.java
+++ src/main/java/no/fint/dfo/handler/administrasjon/personal/GetAllPersonalressursHandler.java
@@ -2,17 +2,50 @@ package no.fint.dfo.handler.administrasjon.personal;
 
 import no.fint.dfo.handler.Handler;
 import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
 import no.fint.model.administrasjon.personal.PersonalActions;
+import no.fint.model.administrasjon.personal.Personalressurs;
+import no.fint.model.felles.Person;
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+import no.fint.model.felles.kompleksedatatyper.Periode;
 import no.fint.model.resource.FintLinks;
+import no.fint.model.resource.Link;
+import no.fint.model.resource.administrasjon.personal.PersonalressursResource;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.Set;
 
 @Service
 public class GetAllPersonalressursHandler implements Handler {
     @Override
-    public void accept(Event<FintLinks> fintLinksEvent) {
+    public void accept(Event<FintLinks> response) {
+        PersonalressursResource personalressursResource = new PersonalressursResource();
+
+        Identifikator ansattnummer = new Identifikator();
+        ansattnummer.setIdentifikatorverdi("123456");
+        personalressursResource.setAnsattnummer(ansattnummer);
+
+        Identifikator systemId = new Identifikator();
+        systemId.setIdentifikatorverdi("61122334455");
+        personalressursResource.setSystemId(systemId);
+
+        personalressursResource.setJobbtittel("Rådgiver");
+        Periode ansettelsesperiode = new Periode();
+        ansettelsesperiode.setStart(new Date());
+        personalressursResource.setAnsettelsesperiode(ansettelsesperiode);
+
+        // ink.with(Klassifikasjonssystem.class, "systemid", input.getClassificationSystemId())
+        personalressursResource.addPerson(
+                Link.with(
+                        Person.class,
+                        "fodselsnummer",
+                        "12345678987")
+        );
+
+        response.setData(Collections.singletonList(personalressursResource));
+        response.setResponseStatus(ResponseStatus.ACCEPTED);
 
     }
 
--- src/main/java/no/fint/dfo/handler/felles/GetAllPersonHandler.java
+++ src/main/java/no/fint/dfo/handler/felles/GetAllPersonHandler.java
@@ -2,8 +2,14 @@ package no.fint.dfo.handler.felles;
 
 import no.fint.dfo.handler.Handler;
 import no.fint.event.model.Event;
+import no.fint.event.model.ResponseStatus;
+import no.fint.model.administrasjon.personal.Personalressurs;
 import no.fint.model.felles.FellesActions;
+import no.fint.model.felles.kompleksedatatyper.Identifikator;
+import no.fint.model.felles.kompleksedatatyper.Personnavn;
 import no.fint.model.resource.FintLinks;
+import no.fint.model.resource.Link;
+import no.fint.model.resource.felles.PersonResource;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -12,7 +18,24 @@ import java.util.Set;
 @Service
 public class GetAllPersonHandler implements Handler {
     @Override
-    public void accept(Event<FintLinks> fintLinksEvent) {
+    public void accept(Event<FintLinks> response) {
+
+        PersonResource personResource = new PersonResource();
+
+        Identifikator nin = new Identifikator();
+        nin.setIdentifikatorverdi("12345678987");
+        personResource.setFodselsnummer(nin);
+
+        Personnavn navn = new Personnavn();
+        navn.setFornavn("Ole");
+        navn.setEtternavn("Olsen");
+        personResource.setNavn(navn);
+
+        personResource.addPersonalressurs(Link.with(Personalressurs.class, "ansattnummer", "123456"));
+        personResource.addPersonalressurs(Link.with(Personalressurs.class, "systemid", "61122334455"));
+
+        response.setData(Collections.singletonList(personResource));
+        response.setResponseStatus(ResponseStatus.ACCEPTED);
 
     }
 
